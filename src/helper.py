e2b130118921140b17ff38af7f3836e0dcd0a4a6
deepak kawate <dipak.kavate947@gmail.com>
2026-03-06 10:22:05 +0530

    Update helper.py

--- src/helper.py
+++ src/helper.py
@@ -12,7 +12,7 @@ from langchain_community.vectorstores import FAISS #📦 FAISS = fast search box
 #Stores text embeddings and helps find similar text.
 
 from langchain_classic.chains import ConversationalRetrievalChain
-from langchain_community.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferMemory
 
 from langchain_groq import ChatGroq #This is your AI brain (LLM) running on Groq servers.
 
