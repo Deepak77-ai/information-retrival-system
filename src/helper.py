f3eb9ae3613c7a844b9043c1143e8d9639d604fb
deepak kawate <dipak.kavate947@gmail.com>
2026-03-06 11:41:46 +0530

    Update helper.py

--- src/helper.py
+++ src/helper.py
@@ -17,8 +17,9 @@ from langchain_groq import ChatGroq
 load_dotenv()
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-os.environ["GROQ_API_KEY"] = GROQ_API_KEY
 
+if GROQ_API_KEY:
+    os.environ["GROQ_API_KEY"] = GROQ_API_KEY
 
 def get_pdf_text(pdf_docs):
     text = ""
