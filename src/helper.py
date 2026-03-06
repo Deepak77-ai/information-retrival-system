dfa35d5f8c8d33a59705366b8f2be8f42b781248
deepak kawate <dipak.kavate947@gmail.com>
2026-03-06 12:03:33 +0530

    Update helper.py

--- src/helper.py
+++ src/helper.py
@@ -13,43 +13,55 @@ from langchain.memory import ConversationBufferMemory
 
 from langchain_groq import ChatGroq
 
-
 load_dotenv()
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
-if GROQ_API_KEY:
-    os.environ["GROQ_API_KEY"] = GROQ_API_KEY
 
 def get_pdf_text(pdf_docs):
     text = ""
+
     for pdf in pdf_docs:
         pdf_reader = PdfReader(pdf)
+
         for page in pdf_reader.pages:
-            text += page.extract_text()
+            page_text = page.extract_text()
+
+            if page_text:
+                text += page_text
+
     return text
 
 
 def get_text_chunks(text):
+
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=20
     )
+
     chunks = text_splitter.split_text(text)
+
     return chunks
 
 
 def get_vectorstore(chunks):
+
     embeddings = HuggingFaceEmbeddings()
-    vectorstore = FAISS.from_texts(texts=chunks, embedding=embeddings)
+
+    vectorstore = FAISS.from_texts(
+        texts=chunks,
+        embedding=embeddings
+    )
+
     return vectorstore
 
 
- def get_conversational_chain(vectorstore):
+def get_conversational_chain(vectorstore):
 
     llm = ChatGroq(
         model="llama3-8b-8192",
-        groq_api_key=os.getenv("GROQ_API_KEY")
+        groq_api_key=GROQ_API_KEY
     )
 
     memory = ConversationBufferMemory(
